e22ddc74f607eea0172b20ee5dd29455e7ab64c8
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-03-13 14:45:38 +0300

    Create a docker-compose file

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/isnakolah/todoAPI/config"
 	"github.com/isnakolah/todoAPI/migration"
@@ -18,7 +21,7 @@ func main() {
 	router := route.SetupRoutes()
 
 	port := os.Getenv("PORT")
-	if port = "" {
+	if port == "" {
 		port = "8080"
 	}
 	if err := router.Run(":" + port); err != nil {
