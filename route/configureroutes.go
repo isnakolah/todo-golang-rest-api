58ad04daa94e9f98885c4bab37164f742bfa9cea
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-03-13 14:20:14 +0300

    Setup routes for the handlers

---
+++ route/configureroutes.go
@@ -0,0 +1,44 @@
+package route
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/isnakolah/todoAPI/auth"
+	"github.com/isnakolah/todoAPI/controller"
+)
+
+func SetupRoutes() *gin.Engine {
+	router := gin.Default()
+	authMiddleware, err := auth.SetupAuth()
+
+	if err != nil {
+		log.Fatal("JWT Error", +err.Error())
+	}
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Welcome to my Todo App")
+	})
+
+	v1 := router.group("/v1")
+	{
+		v1.POST("/login", authMiddleware.LoginHandler)
+
+		v1.POST("/register", controller.RegisterEndpoint)
+
+		todo := v1.group("/todo")
+		{
+			todo.POST("/create", authMiddleware.MiddlewareFunc(), controller.CreateTask)
+			todo.POST("/all", authMiddleware.MiddlewareFunc(), controller.FetchAllTask)
+			todo.POST("/get/:id", authMiddleware.MiddlewareFunc(), controller.FetchSingleTask)
+			todo.POST("/update/:id", authMiddleware.MiddlewareFunc(), controller.UpdateTask)
+			todo.POST("/delete/:id", authMiddleware.MiddlewareFunc(), controller.DeleteTask)
+		}
+	}
+
+	authorization := router.Group("/auth")
+	authorization.GET("/refresh_token", authMiddleware.RefreshHandler)
+
+	return router
+}
