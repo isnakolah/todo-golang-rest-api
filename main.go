6f6cb3e205e3c29ecf39331905e9056a3aea49ed
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-03-13 14:20:27 +0300

    Set up the main.go file as entry for app

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/isnakolah/todoAPI/config"
+	"github.com/isnakolah/todoAPI/migration"
+	"github.com/isnakolah/todoAPI/route"
+)
+
+func init() {
+	db := config.Init()
+	migration.Migrate(db)
+}
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+
+	router := route.SetupRoutes()
+
+	port := os.Getenv("PORT")
+	if port = "" {
+		port = "8080"
+	}
+	if err := router.Run(":" + port); err != nil {
+		log.Panicf("error: %s", err)
+	}
+}
