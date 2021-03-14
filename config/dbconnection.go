f9b480a6e7e1f60c348145992e2a0ee41b92b0c5
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-03-14 03:14:39 +0300

    Update db-connection string

--- config/dbconnection.go
+++ config/dbconnection.go
@@ -1,12 +1,16 @@
 package config
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+)
 
 var DB *gorm.DB
 
 func Init() *gorm.DB {
 	db, err := gorm.Open("postgres",
-		"host=localhost port=5432 user=postgres password=postgres dbname=todogolang sslmode=disable")
+		"host=postgres port=5432 user=postgres password=postgres dbname=todogolang sslmode=disable")
+	// "docker:password@tcp(postgrestodo)/todogolang"
 
 	if err != nil {
 		panic(err.Error())
